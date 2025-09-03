0ad3eb05085b19c0fbb060a130381eb68d433f4b
Ian Chan <ianchan1108@hotmail.com>
2025-09-02 22:49:03 -0700

    add messages globally

---
+++ lang/messages/en/user.js
@@ -0,0 +1,17 @@
+// Messages for the Memory Game
+const messages = {
+  // UI Text
+  buttonPrompt: "How many buttons to create?",
+  inputPlaceholder: "Enter a number (3-7)",
+  goButton: "Go!",
+
+  // Game Messages
+  excellentMemory: "Excellent memory!",
+  wrongOrder: "Wrong order!",
+
+  // Validation Messages
+  invalidRange: "Please enter a number between 3-7",
+};
+
+// Makes the messages object available globally
+window.messages = messages;
