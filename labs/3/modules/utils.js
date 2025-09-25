664bb59a2c2a36e12514fac776f24c4a2954bc5b
Ian Chan <ianchan1108@hotmail.com>
2025-09-24 22:07:06 -0700

    create getDate, writeFile, readFile functions

--- labs/3/modules/utils.js
+++ labs/3/modules/utils.js
@@ -1,5 +1,30 @@
-function getDate() {
-    return new Date();
-}
+const fs = require("fs").promises;
 
-exports.getDate = getDate;
+exports.getDate = () => {
+  return new Date();
+};
+
+exports.writeFile = async (data, query) => {
+  let fileHandle;
+  let text = data.text;
+
+  try {
+    fileHandle = await fs.open("file.txt", "a");
+    await fileHandle.appendFile(text);
+  } catch (err) {
+    console.error(`Error: ${err}`);
+  } finally {
+    if (fileHandle) {
+      await fileHandle.close();
+    }
+  }
+};
+
+exports.readFile = async (query) => {
+  try {
+    const data = await fs.readFile(query, "utf-8");
+    return data;
+  } catch (err) {
+    console.error(`Error reading file: ${err}`);
+  }
+};
